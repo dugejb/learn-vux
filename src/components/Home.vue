cb67e8c1e080690e1d85e0259386d0494b751296
dugejb <597908115@qq.com>
2018-01-08 16:09:28 +0800

    add

---
+++ src/components/Home.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="vux-demo">
+      <swiper :list="swiperlist" loop></swiper>
+    </div>
+    <group title="cell demo">
+      <cell title="VUX" value="cool" is-link link="/hello"></cell>
+      <x-button type="primary" @click.native="showPosition('')">hi</x-button>
+    </group>
+    <toast v-model="showPositionValue" type="text" :time="800" is-show-mask :text="msg" :position="position"></toast>
+  </div>
+</template>
+
+<script>
+import { Swiper, Group, Cell, XButton, Toast } from "vux";
+
+export default {
+  components: {
+    Swiper,
+    Group,
+    Cell,
+    XButton,
+    Toast
+  },
+  methods: {
+    showPosition(position) {
+      this.position = position;
+      this.showPositionValue = true;
+    }
+  },
+  data() {
+    return {
+      swiperlist: [
+        {
+          url: "javascript:",
+          img: "../../assets/1.jpg"
+        },
+        {
+          url: "javascript:",
+          img: "../../assets/2.jpg"
+        },
+        {
+          url: "javascript:",
+          img: "../../assets/3.jpg"
+        }
+      ],
+      position: "default",
+      showPositionValue: false,
+      msg: "Hello zmy!"
+    };
+  }
+};
+</script>
+
+<style>
+.vux-demo {
+  margin-top: 45px;
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px;
+}
+</style>
---
+++ src/components/Me.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="me">
+    <group>
+          
+      <cell title="Watch Wechat" is-link>
+        <img slot="icon" width="20" style="display:block;margin-right:5px;" src="../../assets/applewatch.png">
+      </cell>
+    </group>
+  </div>
+</template>
+
+<script>
+import { Group, Cell } from "vux";
+export default {
+  components: {
+    Group,
+    Cell
+  }
+};
+</script>
+
+<style>
+
+</style>
+
+<style>
+.me {
+  margin-top: 60px;
+}
+</style>
